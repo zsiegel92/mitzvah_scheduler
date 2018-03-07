492ee47c17087a319ccf98ec38965a824c12cf77
Zach S <zsiegel92@gmail.com>
2018-03-07 13:25:38 -0800

    static

--- static/main.bundle.js
+++ static/main.bundle.js
@@ -25,6 +25,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("./node_modules/@angular/router/esm5/router.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__form_form_component__ = __webpack_require__("./src/app/form/form.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__not_found_not_found_component__ = __webpack_require__("./src/app/not-found/not-found.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -35,8 +36,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 // import { AppComponent } from './app.component';
 
+
 var routes = [
-    { path: 'form', component: __WEBPACK_IMPORTED_MODULE_2__form_form_component__["a" /* FormComponent */] }
+    { path: 'form', component: __WEBPACK_IMPORTED_MODULE_2__form_form_component__["a" /* FormComponent */] },
+    { path: '**', component: __WEBPACK_IMPORTED_MODULE_3__not_found_not_found_component__["a" /* NotFoundComponent */] }
 ];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
@@ -115,6 +118,7 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__data_service__ = __webpack_require__("./src/app/data.service.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__messages_messages_component__ = __webpack_require__("./src/app/messages/messages.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__message_service__ = __webpack_require__("./src/app/message.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__not_found_not_found_component__ = __webpack_require__("./src/app/not-found/not-found.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -131,6 +135,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
@@ -139,7 +144,8 @@ var AppModule = /** @class */ (function () {
             declarations: [
                 __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
                 __WEBPACK_IMPORTED_MODULE_6__form_form_component__["a" /* FormComponent */],
-                __WEBPACK_IMPORTED_MODULE_8__messages_messages_component__["a" /* MessagesComponent */]
+                __WEBPACK_IMPORTED_MODULE_8__messages_messages_component__["a" /* MessagesComponent */],
+                __WEBPACK_IMPORTED_MODULE_10__not_found_not_found_component__["a" /* NotFoundComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
@@ -252,7 +258,7 @@ module.exports = ""
 /***/ "./src/app/form/form.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  form works!\n</p>\n\n<h3>Schools</h3>\n<div class=\"grid grid-pad\">\n  <a *ngFor=\"let school of schools\" class=\"col-1-4\">\n    <div class=\"module school\">\n      <h4>{{school.name}}</h4>\n      <ul><li>{{school.id}}</li></ul>\n    </div>\n  </a>\n</div>\n"
+module.exports = "<p>\n\tform works!\n</p>\n<!-- <h3>Schools</h3>\n<div class=\"grid grid-pad\">\n\t<a *ngFor=\"let school of schools\" class=\"col-1-4\">\n\t\t<div class=\"module school\">\n\t\t\t<h4>{{school.name}}</h4>\n\t\t\t<ul><li>{{school.id}}</li></ul>\n\t\t</div>\n\t</a>\n</div>\n-->\n<h3>Student Information</h3>\n<p>Please fill in the information below to help Sinai Temple plan and schedule your child's B'nai Mitzvah\n\tForms submitted by March 15 th 2018 will be given first preference in terms of dates and venue selection.\nFor assistance submitting this information, please contact [CONTACT EMAIL FOR PLANNING]</p>\n<br>\n<br>\n<div class=\"form-group\">\n\t<label for=\"name\">Email Address</label>\n\t<input type=\"text\" class=\"form-control\" id=\"name\" required>\n</div>\n"
 
 /***/ }),
 
@@ -387,6 +393,57 @@ var MessagesComponent = /** @class */ (function () {
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__message_service__["a" /* MessageService */]])
     ], MessagesComponent);
     return MessagesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/not-found/not-found.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/not-found/not-found.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "What do you want to do?\n<ul>\n\t<li><a routerLink='/form'>Submit your child's information</a></li>\n\t<li><a routerLink='/'>View current TENTATIVE B'nai Mitzvah schedule (not set up yet)</a></li>\n</ul>\n"
+
+/***/ }),
+
+/***/ "./src/app/not-found/not-found.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NotFoundComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+// import { routes } from '../app-routing.module'
+var NotFoundComponent = /** @class */ (function () {
+    function NotFoundComponent() {
+    }
+    NotFoundComponent.prototype.ngOnInit = function () {
+    };
+    NotFoundComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-not-found',
+            template: __webpack_require__("./src/app/not-found/not-found.component.html"),
+            styles: [__webpack_require__("./src/app/not-found/not-found.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], NotFoundComponent);
+    return NotFoundComponent;
 }());
 
 
