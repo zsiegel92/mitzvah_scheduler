5c60d814550ac6f691bf538018ada06b43919135
Zach S <zsiegel92@gmail.com>
2018-03-19 15:50:50 -0700

    models

--- models.py
+++ models.py
@@ -1,13 +1,37 @@
 from database import db
 
 
+class Student(db.Model):
+	__tablename__ = "students"
+	id = db.Column(db.Integer,autoincrement=True,unique=True)
+	email=db.Column(db.String(),primary_key=True)
+	childName=db.Column(db.String(),primary_key=True)
+	school = db.relationship("School",back_populates ="students",uselist=False)
+	hebSchool = db.relationship("HebSchool",back_populates ="students",uselist=False) #way to make non-list?
+	dob  = db.Column(db.DateTime())
+	ranking_main=db.Column(db.Integer())
+	ranking_familyMinyan=db.Column(db.Integer())
+	ranking_torahInTheRound=db.Column(db.Integer())
+	atVenue = db.Column(db.Boolean())
+	nonDates = db.relationship("nonDates",back_populates="student",uselist=True)
+	accommodation = db.Column(db.Text)
+	twin = db.Column(db.Boolean())
+	school_id = db.Column(db.Integer,db.ForeignKey('schools.id'))
+	hebSchool_id = db.Column(db.Integer,db.ForeignKey('hebschools.id'))
+
+
+class nonDates(db.Model):
+	id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+	date = db.Column(db.DateTime())
+	student = db.relationship("Student",back_populates="nonDates",uselist=False)
+	student_id = db.Column(db.Integer,db.ForeignKey('students.id'))
 
 class School(db.Model):
 	__tablename__ = 'schools'
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String())
-
+	students = db.relationship("Student",uselist=True,back_populates="school")
 	def __init__(self, name):
 		self.name = name
 
@@ -23,6 +47,7 @@ class HebSchool(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String())
+	students = db.relationship("Student",back_populates="hebSchool")
 
 	def __init__(self, name):
 		self.name = name
