be10ecf35f35f8c49308c6158d06b7db9f4d7235
Zach S <zsiegel92@gmail.com>
2018-03-20 12:31:23 -0700

    flask shell context

--- manage.py
+++ manage.py
@@ -5,14 +5,20 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
+from models import Student, School,HebSchool,NonDate
+
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+@manager.shell
+def make_shell_context():
+	return dict(app=app, db=db, Student=Student,School=School,HebSchool=HebSchool,NonDate=NonDate)
+
 @manager.command
 def populate():
-	from models import School,HebSchool
+	# from models import School,HebSchool
 	names = ['Lainer School']
 	for name in names:
 		school = School.query.filter_by(name=name).first()
@@ -27,9 +33,10 @@ def populate():
 			school = HebSchool(name=name)
 		db.session.add(school)
 	db.session.commit()
+
 @manager.command
 def delete_all():
-	from models import School,Student
+	# from models import School,Student
 	for s in Student.query.all():
 	 	db.session.delete(s)
 	db.session.commit()
